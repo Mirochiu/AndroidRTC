1154974451c4d1568f4111c3aa81a705058c05c4
Chiu, Yung-Hsiang <mirror.tw@gmail.com>
2018-03-07 17:22:10 +0800

    Leaving IncomingCallActivity when the caller hangup

--- app/src/main/java/me/kevingleason/androidrtc/IncomingCallActivity.java
+++ app/src/main/java/me/kevingleason/androidrtc/IncomingCallActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,15 +13,20 @@ import android.widget.Toast;
 
 import com.pubnub.api.Callback;
 import com.pubnub.api.Pubnub;
+import com.pubnub.api.PubnubError;
+import com.pubnub.api.PubnubException;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import me.kevingleason.androidrtc.util.Constants;
 import me.kevingleason.pnwebrtc.PnPeerConnectionClient;
+import me.kevingleason.pnwebrtc.PnRTCMessage;
 
 
 public class IncomingCallActivity extends Activity {
+    static String TAG = "IncomingCall";
+
     private SharedPreferences mSharedPreferences;
     private String username;
     private String callUser;
@@ -57,8 +63,59 @@ public class IncomingCallActivity extends Activity {
 
         this.mPubNub  = new Pubnub(Constants.PUB_KEY, Constants.SUB_KEY);
         this.mPubNub.setUUID(this.username);
+
+        // Subscribe my channel for receiving the hangup msg from caller
+        try {
+            this.mPubNub.subscribe(this.username, new Callback() {
+                @Override
+                public void successCallback(String channel, Object message) {
+                    if (!(message instanceof JSONObject)) {
+                        return; // Ignore if not JSONObject
+                    }
+                    JSONObject jsonMsg = (JSONObject) message;
+                    try {
+                        if (!jsonMsg.has(PnRTCMessage.JSON_NUMBER) ||
+                                !jsonMsg.has(PnRTCMessage.JSON_PACKET)) {
+                            return;     //Ignore not hangup messages.
+                        }
+                        if (!callUser.equals(jsonMsg.get(PnRTCMessage.JSON_NUMBER))) {
+                            return;     //Ignore not hangup messages.
+                        }
+                        JSONObject pktMsg = jsonMsg.getJSONObject(PnRTCMessage.JSON_PACKET);
+                        if (pktMsg.has(PnRTCMessage.JSON_HANGUP) &&
+                                pktMsg.getBoolean(PnRTCMessage.JSON_HANGUP)) {
+                            showToast("Caller hangup!");
+                            Intent intent = new Intent(IncomingCallActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                    } catch (JSONException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void connectCallback(String channel, Object message) {
+                    Log.d(TAG,"my channel CONNECTED: " + message.toString());
+                }
+
+                @Override
+                public void errorCallback(String channel, PubnubError error) {
+                    Log.d(TAG,"my channel ERROR: " + error.toString());
+                }
+            });
+        } catch (PubnubException e){
+            e.printStackTrace();
+        }
     }
 
+    private void showToast(final String message){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(IncomingCallActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
